bc661b39785a66b316bba9ca6efab41e94505211
Teguh Budi Santoso <86312158+Tiktodz@users.noreply.github.com>
2021-10-04 15:53:22 +0700

    revise again

--- drivers/power/supply/qcom/smb1351-charger.c
+++ drivers/power/supply/qcom/smb1351-charger.c
@@ -523,8 +523,7 @@ struct irq_handler_info {
 /* USB input charge current */
 static int usb_chg_current[] = {
 	1500, 1600,
-	1700, 1800, 2000, 2200, 2500, 3000, 3400, 3600, 3800,
-	4000, 4500,
+	1700, 1800, 2000, 2200, 2500, 3000,
 };
 
 static int fast_chg_current[] = {
